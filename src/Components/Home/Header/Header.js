dab3e1a9e06e94977c1660ca2151d4841c4b2819
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-07-03 23:33:50 +0600

    clip-path add

---
+++ src/Components/Home/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navber from '../../Seared/Navber/Navber'
+import "./Heaser.css"
+
+function Header() {
+  return (
+    <header>
+      <div className="wrap">
+        <Navber />
+        .
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from '../Components/Home/Header/Header'
+
+
 
 function Home() {
   return (
     <>
-     <h1>Home</h1> 
+      <Header />
     </>
   )
 }
